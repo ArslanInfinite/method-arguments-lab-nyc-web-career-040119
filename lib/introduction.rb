5c8eb0ffde7305dd113488dcd059a6d6eb9ae6bc
Arslan Ali <ali.arslan319@gmail.com>
2019-04-01 17:04:43 +0000

    Automatically backed up by Learn

---
+++ lib/introduction.rb
@@ -0,0 +1,7 @@
+def greeting(name)
+  puts "Hi, my name is #{name}."
+end
+
+def greeting_with_language(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
